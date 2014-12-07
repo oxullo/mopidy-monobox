731fb0a974304eeb8836148ca7f7448a61b06cc9
OXullo Intersecans <x@brainrapers.org>
2014-12-07 20:24:51 +0100

    Added seek, power control handled with states

--- mopidy_monobox/frontend.py
+++ mopidy_monobox/frontend.py
@@ -10,7 +10,15 @@ import pykka
 from mopidy import core
 from smc import SerialMonoboxController
 
-ENCODER_PULSES_THRESHOLD = 15
+ENCODER_PULSES_THRESHOLD = 30
+
+STATE_POWER_STANDBY = 'STATE_POWER_STANDBY'
+STATE_POWER_ON = 'STATE_POWER_ON'
+STATE_POWER_PENDING_ON = 'STATE_POWER_PENDING_ON'
+
+STATE_TRACK_PLAYBACK_IDLE = 'STATE_TRACK_PLAYBACK_IDLE'
+STATE_TRACK_PLAYBACK_PENDING = 'STATE_TRACK_PLAYBACK_PENDING'
+STATE_TRACK_PLAYBACK_PLAYING = 'STATE_TRACK_PLAYBACK_PLAYING'
 
 logger = logging.getLogger(__name__)
 
@@ -20,15 +28,20 @@ class MonoboxFrontend(pykka.ThreadingActor, core.CoreListener):
         self.core = core
         self.encoder_abspos = 0
         self.playlists_ready = False
-        self.pending_power = False
+        self.power_state = STATE_POWER_STANDBY
+        self.track_state = STATE_TRACK_PLAYBACK_IDLE
 
         self.smc = SerialMonoboxController(self, config['monobox']['serial_port'])
 
     def playlists_loaded(self):
         self.playlists_ready = True
-        if self.pending_power:
+        if self.power_state == STATE_POWER_PENDING_ON:
             self.power_on()
-            self.pending_power = None
+
+    def track_playback_started(self, tl_track):
+        logger.info('track_playback_started: %s' % tl_track)
+        self.encoder_abspos = 0
+        self.track_state = STATE_TRACK_PLAYBACK_PLAYING
 
     def set_power_control(self, wanted_state):
         logger.info('set_power_control wanted_state=%d' % wanted_state)
@@ -37,7 +50,7 @@ class MonoboxFrontend(pykka.ThreadingActor, core.CoreListener):
                 self.power_on()
             else:
                 logger.debug('Delaying power on')
-                self.pending_power = True
+                self.power_state = STATE_POWER_PENDING_ON
         else:
             self.standby()
 
@@ -47,12 +60,20 @@ class MonoboxFrontend(pykka.ThreadingActor, core.CoreListener):
         for playlist in playlists:
             self.core.tracklist.add(uri=playlist.uri)
         self.core.playback.play()
+        self.power_state = STATE_POWER_ON
 
     def standby(self):
         if self.core.playback.state.get() == core.PlaybackState.PLAYING:
+            self.track_state = STATE_TRACK_PLAYBACK_IDLE
             self.core.playback.pause()
 
     def update_encoder(self, delta):
+        if (self.track_state == STATE_TRACK_PLAYBACK_PENDING or
+                self.core.playback.state.get() != core.PlaybackState.PLAYING):
+            return
+
+        self.recalculate_encoder_position()
+
         self.encoder_abspos += delta
 
         if self.encoder_abspos >= ENCODER_PULSES_THRESHOLD:
@@ -61,11 +82,25 @@ class MonoboxFrontend(pykka.ThreadingActor, core.CoreListener):
         elif self.encoder_abspos <= -ENCODER_PULSES_THRESHOLD:
             self.play_previous()
             self.encoder_abspos = 0
-        
+        elif (self.encoder_abspos >= 0 and
+                self.core.playback.state.get() == core.PlaybackState.PLAYING):
+            norm_pos = float(self.encoder_abspos) / ENCODER_PULSES_THRESHOLD
+            current_track = self.core.playback.current_track.get()
+            seek_ms = int(current_track.length * norm_pos)
+            self.core.playback.seek(seek_ms)
+
+    def recalculate_encoder_position(self):
+        current_track = self.core.playback.current_track.get()
+        norm_pos = float(self.core.playback.time_position.get()) / current_track.length
+        new_encoder_pos = int(ENCODER_PULSES_THRESHOLD * norm_pos)
+        self.encoder_abspos = new_encoder_pos
+
     def play_next(self):
         logger.debug('play_next')
+        self.track_state = STATE_TRACK_PLAYBACK_PENDING
         self.core.playback.next()
 
     def play_previous(self):
         logger.debug('play_previous')
+        self.track_state = STATE_TRACK_PLAYBACK_PENDING
         self.core.playback.previous()
