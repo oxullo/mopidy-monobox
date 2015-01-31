488a8e64843f09469bdac476af8b229e3a1f8838
OXullo Intersecans <x@brainrapers.org>
2015-01-31 18:16:54 +0100

    Heavy rework, encoder removed, testing the one-button approach

--- mopidy_monobox/frontend.py
+++ mopidy_monobox/frontend.py
@@ -12,7 +12,6 @@ from smc import SerialMonoboxController
 
 STATE_POWER_STANDBY = 'STATE_POWER_STANDBY'
 STATE_POWER_ON = 'STATE_POWER_ON'
-STATE_POWER_PENDING_ON = 'STATE_POWER_PENDING_ON'
 
 STATE_TRACK_PLAYBACK_IDLE = 'STATE_TRACK_PLAYBACK_IDLE'
 STATE_TRACK_PLAYBACK_PENDING = 'STATE_TRACK_PLAYBACK_PENDING'
@@ -25,10 +24,14 @@ class MonoboxFrontend(pykka.ThreadingActor, core.CoreListener):
         super(MonoboxFrontend, self).__init__()
         self.core = core
         self.config = config
-        self.encoder_abspos = 0
-        self.playlists_ready = False
         self.power_state = STATE_POWER_STANDBY
         self.track_state = STATE_TRACK_PLAYBACK_IDLE
+
+        self.core.tracklist.clear()
+        self.core.tracklist.consume = False
+        for radio in self.core.library.browse(uri='tunein:category:local').get():
+            self.core.tracklist.add(uri=radio.uri)
+            logger.debug('Added radio %s' % str(radio))
 
         self.smc = SerialMonoboxController.start(self,
                 config['monobox']['serial_port'],
@@ -37,10 +40,8 @@ class MonoboxFrontend(pykka.ThreadingActor, core.CoreListener):
     def on_stop(self):
         self.smc.stop()
 
-    def playlists_loaded(self):
-        self.playlists_ready = True
-        if self.power_state == STATE_POWER_PENDING_ON:
-            self.power_on()
+    def playback_state_changed(self, old_state, new_state):
+        print 'PBACK STATE:', old_state, new_state
 
     def track_playback_started(self, tl_track):
         logger.info('track_playback_started: %s' % tl_track)
@@ -53,32 +54,11 @@ class MonoboxFrontend(pykka.ThreadingActor, core.CoreListener):
     def set_power_control(self, wanted_state):
         logger.info('set_power_control wanted_state=%d' % wanted_state)
         if wanted_state == 1:
-            if self.playlists_ready:
-                self.power_on()
-            else:
-                logger.debug('Delaying power on')
-                self.power_state = STATE_POWER_PENDING_ON
+            self.power_on()
         else:
             self.standby()
 
     def power_on(self):
-        self.core.tracklist.clear()
-        playlists = self.core.playlists.playlists.get()
-
-        wanted_playlists = self.config['monobox']['only_playlists']
-        for playlist in playlists:
-            if ((wanted_playlists and playlist.name in wanted_playlists) or
-                    not wanted_playlists):
-                logger.info('Adding playlist %s' % playlist.name)
-                self.core.tracklist.add(uri=playlist.uri)
-            else:
-                logger.info('Skipping playlist %s' % playlist.name)
-
-        logger.info('Loaded %d tracks' % self.core.tracklist.length.get())
-        if self.config['monobox']['shuffle']:
-            logger.info('Shuffling')
-            self.core.tracklist.shuffle()
-
         self.core.playback.play()
         self.power_state = STATE_POWER_ON
 
@@ -86,54 +66,22 @@ class MonoboxFrontend(pykka.ThreadingActor, core.CoreListener):
         self.power_state = STATE_POWER_STANDBY
         if self.core.playback.state.get() == core.PlaybackState.PLAYING:
             self.track_state = STATE_TRACK_PLAYBACK_IDLE
-            self.core.playback.pause()
-
-    def update_encoder(self, delta):
-        if (self.track_state == STATE_TRACK_PLAYBACK_PENDING or
-                self.core.playback.state.get() != core.PlaybackState.PLAYING):
-            return
-
-        if self.config['monobox']['cue_feature']:
-            self.cue(delta)
-        else:
-            self.encoder_abspos += delta
-            if self.encoder_abspos > self.config['monobox']['pulses_trigger']:
-                self.encoder_abspos = 0
-                self.play_next()
-            elif self.encoder_abspos < -self.config['monobox']['pulses_trigger']:
-                self.encoder_abspos = 0
-                self.play_previous()
-
-    def recalculate_encoder_position(self):
-        current_track = self.core.playback.current_track.get()
-        norm_pos = float(self.core.playback.time_position.get()) / current_track.length
-        new_encoder_pos = int(self.config['monobox']['pulses_trigger'] * norm_pos)
-        self.encoder_abspos = new_encoder_pos
-
-    def cue(self, delta):
-        self.recalculate_encoder_position()
-
-        self.encoder_abspos += delta
-
-        if self.encoder_abspos >= self.config['monobox']['pulses_trigger']:
-            self.play_next()
-            self.encoder_abspos = 0
-        elif self.encoder_abspos <= -self.config['monobox']['pulses_trigger']:
-            self.play_previous()
-            self.encoder_abspos = 0
-        elif (self.encoder_abspos >= 0 and
-                self.core.playback.state.get() == core.PlaybackState.PLAYING):
-            norm_pos = float(self.encoder_abspos) / self.config['monobox']['pulses_trigger']
-            current_track = self.core.playback.current_track.get()
-            seek_ms = int(current_track.length * norm_pos)
-            self.core.playback.seek(seek_ms)
+            self.core.playback.stop()
 
     def play_next(self):
-        logger.debug('play_next')
+        logger.info('play_next')
         self.track_state = STATE_TRACK_PLAYBACK_PENDING
         self.core.playback.next()
 
     def play_previous(self):
-        logger.debug('play_previous')
+        logger.info('play_previous')
         self.track_state = STATE_TRACK_PLAYBACK_PENDING
         self.core.playback.previous()
+
+    def next_button_pressed(self):
+        if (self.track_state == STATE_TRACK_PLAYBACK_PENDING or
+                self.core.playback.state.get() != core.PlaybackState.PLAYING):
+            logger.warning('Skipping next button request')
+            return
+        else:
+            self.play_next()
--- mopidy_monobox/smc.py
+++ mopidy_monobox/smc.py
@@ -3,10 +3,10 @@
 
 from __future__ import unicode_literals
 
+import threading
 import re
 import logging
 import serial
-import gobject
 import pykka
 
 from mopidy import exceptions
@@ -19,32 +19,46 @@ class SerialMonoboxController(pykka.ThreadingActor):
     def __init__(self, frontend, serial_port, serial_bps):
         super(SerialMonoboxController, self).__init__()
         try:
-            self.s = serial.Serial(serial_port, serial_bps)
+            self.s = serial.Serial(serial_port, serial_bps, timeout=0.5)
         except Exception as error:
             raise exceptions.FrontendError('SMC serial connection failed: %s' %
                     encoding.locale_decode(error))
 
-        self.s.nonblocking()
-        self.frontend_proxy = frontend.actor_ref.proxy()
+        self.frontend = frontend
         self.buffer = ''
 
     def on_start(self):
         self.s.flushInput()
-        gobject.io_add_watch(self.s.fileno(),
-                gobject.IO_IN | gobject.IO_ERR | gobject.IO_HUP,
-                self.read_chunk)
+        thread = threading.Thread(target=self.thread_run)
+        thread.start()
+
+    def on_stop(self):
+        self.running = False
+
+    def thread_run(self):
+        self.running = True
+        while self.running:
+            ch = self.s.read()
+            if ch != '\r':
+                self.buffer += ch
+
+            # logger.debug('SMC buf: %s' % str([c for c in self.buffer]))
+
+            while '\n' in self.buffer:
+                self.process_line(self.buffer[0:self.buffer.find('\n')])
+                self.buffer = self.buffer[self.buffer.find('\n') + 1:]
 
     def process_parsed(self, typ, value):
         if typ == 'P':
-            self.frontend_proxy.set_power_control(value)
-        elif typ == 'E':
-            self.frontend_proxy.update_encoder(value)
+            self.frontend.set_power_control(value)
         elif typ == 'V':
-            self.frontend_proxy.set_volume(value)
+            self.frontend.set_volume(value)
+        elif typ == 'B' and value == 1:
+            self.frontend.next_button_pressed()
 
     def process_line(self, line):
         logger.debug('SMC process line: %s' % line)
-        res = re.search(r'^([EBPV]):(\-?\d+)$', line)
+        res = re.search(r'^([BPV]):(\-?\d+)$', line)
         if res:
             typ, value = res.groups()
             try:
@@ -53,23 +67,3 @@ class SerialMonoboxController(pykka.ThreadingActor):
                 logger.warning('Cannot decode value %s (line=%s)' % (value, line))
             else:
                 self.process_parsed(typ, value)
-
-    def read_chunk(self, fd, flags):
-        logger.debug('SCM read_chunk fd=%d flags=%d' % (fd, flags))
-
-        if flags & (gobject.IO_ERR | gobject.IO_HUP):
-            self.stop('Bad client flags: %s' % flags)
-            return True
-
-        while self.s.inWaiting():
-            ch = self.s.read()
-            if ch != '\r':
-                self.buffer += ch
-
-        logger.debug('SMC buf: %s' % str([c for c in self.buffer]))
-
-        while '\n' in self.buffer:
-            self.process_line(self.buffer[0:self.buffer.find('\n')])
-            self.buffer = self.buffer[self.buffer.find('\n') + 1:]
-
-        return True
